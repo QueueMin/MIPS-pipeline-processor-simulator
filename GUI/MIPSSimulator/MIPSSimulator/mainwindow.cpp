0e530a0c93ee0da434a90a95287a055fe6c8f872
QueueMin <okkimok123@kookmin.ac.kr>
2022-11-19 20:40:39 +0900

    resetting GUI env

--- GUI/MIPSSimulator/MIPSSimulator/mainwindow.cpp
+++ GUI/MIPSSimulator/MIPSSimulator/mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "FileEncode.cpp"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->sim = new Simulator();
 }
 
 MainWindow::~MainWindow()
@@ -14,23 +14,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// select file and load for simulator
 void MainWindow::on_openButton_clicked()
 {
     QString fileName = QFileDialog::getOpenFileName(this);
-    encode(fileName.toStdString());
+    this->sim->fileLoad(fileName.toStdString());
+
     ui->instructionTable->clearContents();
 
 
-    for (int i = 0; i < 100; i++){
+    int sPC = binToDec(sim->MManager.getStartPC());
+    for (int i = 0; i < sim->MManager.getFileLength(); i++){
         QTableWidgetItem* pc = new QTableWidgetItem;
         QTableWidgetItem* inst = new QTableWidgetItem;
-        pc->setText(QString::fromStdString(binToHex(startPC)));
-        inst->setText(QString::fromStdString(binToHex(program[i])));
+
+        pc->setText(QString::fromStdString(decToHex(sPC+4*i)));
+        inst->setText(QString::fromStdString(binToHex(sim->MManager.getProgram(decToHex(sPC)))));
+
 
         ui->instructionTable->setItem(i, 0, pc);
         ui->instructionTable->setItem(i, 1, inst);
 
-        startPC = (binToDec(startPC)+4);
     }
 
 
