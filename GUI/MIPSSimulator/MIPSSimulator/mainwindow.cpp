dffdf37dc4d3ccd5c7c7c1b4e0b934697b843eb9
QueueMin <okkimok123@kookmin.ac.kr>
2022-11-23 11:42:16 +0900

    GUI circuit scene done

--- GUI/MIPSSimulator/MIPSSimulator/mainwindow.cpp
+++ GUI/MIPSSimulator/MIPSSimulator/mainwindow.cpp
@@ -16,9 +16,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->actionHex, &QAction::triggered, this, &MainWindow::regToHex);
 
     ui->loadedCiruit->hide();
-    resetImg();
-
-
 
     this->font_K = QFont("Consolas",14,900);
     this->brushK = QColor(0, 0, 0, 255);
@@ -33,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent)
     this->brushY = QColor(225, 200, 0, 255);
     paintY.setBrush(brushY);
 
+    refreshImgInfo();
 }
 
 MainWindow::~MainWindow()
@@ -43,6 +41,8 @@ MainWindow::~MainWindow()
 // select file and load for simulator
 void MainWindow::on_openButton_clicked()
 {
+    delete(sim);
+    sim = new Simulator();
     QString fileName = QFileDialog::getOpenFileName(this);
 //    this->sim->fileLoad(fileName.toStdString());
     sim->fileLoad(fileName.toStdString());
@@ -63,6 +63,7 @@ void MainWindow::on_openButton_clicked()
     }
 
     refreshRegTable(16);
+    refreshImgInfo();
 }
 
 void MainWindow::refreshRegTable(int code){
@@ -152,25 +153,178 @@ void MainWindow::refreshImgInfo(){
     resetImg();
 
     // MEM/WB Register
+    QString MMemtoReg = QString::fromStdString(rbool(sim->MEMWB.MemtoReg));
+    QGraphicsTextItem* gMMemtoReg = this->circuitScene.addText("= "+MMemtoReg);
+    fontB(gMMemtoReg);
+    gMMemtoReg->setPos(1346, 234);
+
+    QString MRegWrite = QString::fromStdString(rbool(sim->MEMWB.RegWrite));
+    QGraphicsTextItem* gMRegWrite = this->circuitScene.addText("RegWrite= "+MRegWrite);
+    fontB(gMRegWrite);
+    gMRegWrite->setPos(1266, 216);
+
+    QString MRd = QString::fromStdString(sim->MEMWB.Rd.to_string());
+    QGraphicsTextItem* gMRd = this->circuitScene.addText(MRd);
+    fontB(gMRd);
+    gMRd->setPos(1196, 616);
+
+    QString Address = QString::fromStdString(binToHex(sim->MEMWB.Address));
+    QGraphicsTextItem* gAddress = this->circuitScene.addText(Address);
+    fontB(gAddress);
+    gAddress->setPos(1188, 384);
+
+    QString MemData = QString::fromStdString(binToHex(sim->MEMWB.Data));
+    QGraphicsTextItem* gMemData = this->circuitScene.addText(MemData);
+    fontB(gMemData);
+    gMemData->setPos(1144, 474);
 
     // EX/MEM Register
 
+    QString ForwardA = QString::fromStdString(std::bitset<2>(sim->ForwardUnit.getForwardA()).to_string());
+    QGraphicsTextItem* gForwardA = this->circuitScene.addText(ForwardA);
+    fontY(gForwardA);
+    gForwardA->setPos(820, 324);
+
+    QString ForwardB = QString::fromStdString(std::bitset<2>(sim->ForwardUnit.getForwardB()).to_string());
+    QGraphicsTextItem* gForwardB = this->circuitScene.addText(ForwardB);
+    fontY(gForwardB);
+    gForwardB->setPos(820, 400);
+
+    QString ExData = QString::fromStdString(binToHex(sim->EXMEM.Data2));
+    QGraphicsTextItem* gExData = this->circuitScene.addText(ExData);
+    fontB(gExData);
+    gExData->setPos(980, 420);
+    QGraphicsTextItem* gExData2 = this->circuitScene.addText(ExData);
+    fontB(gExData2);
+    gExData2->setPos(1096, 76);
+
+    QString ALUResult = QString::fromStdString(binToHex(sim->EXMEM.ALUResult));
+    QGraphicsTextItem* gALUResult = this->circuitScene.addText(ALUResult);
+    fontB(gALUResult);
+    gALUResult->setPos(980, 392);
+
+    QString ERd = QString::fromStdString(sim->EXMEM.Rd.to_string());
+    QGraphicsTextItem* gERd = this->circuitScene.addText(ERd);
+    fontB(gERd);
+    gERd->setPos(996, 616);
+    QGraphicsTextItem* gERd2 = this->circuitScene.addText("= "+ERd);
+    fontB(gERd2);
+    gERd2->setPos(960, 76);
+
+
+    QString EXMemtoReg = QString::fromStdString(rbool(sim->EXMEM.MemtoReg));
+    QGraphicsTextItem* gEXMemtoReg = this->circuitScene.addText("MemToReg=" + EXMemtoReg);
+    fontB(gEXMemtoReg);
+    gEXMemtoReg->setPos(1090, 190);
+
+    QString EXRegWrite = QString::fromStdString(rbool(sim->EXMEM.RegWrite));
+    QGraphicsTextItem* gEXRegWrite = this->circuitScene.addText("="+EXRegWrite);
+    fontB(gEXRegWrite);
+    gEXRegWrite->setPos(1076, 302);
+
+    QString EXMemWrite = QString::fromStdString(rbool(sim->EXMEM.MemWrite));
+    QGraphicsTextItem* gEXMemWrite = this->circuitScene.addText(EXMemWrite);
+    fontB(gEXMemWrite);
+    gEXMemWrite->setPos(1164, 282);
+
+    QString EXMemRead = QString::fromStdString(rbool(sim->EXMEM.MemRead));
+    QGraphicsTextItem* gEXMemRead = this->circuitScene.addText(EXMemRead);
+    fontB(gEXMemRead);
+    gEXMemRead->setPos(1110, 282);
+
+    QGraphicsTextItem* gEXMemRead2 = this->circuitScene.addText("="+EXMemRead);
+    fontB(gEXMemRead2);
+    gEXMemRead2->setPos(918, 20);
+
     // ID/EX Register
-    QString RegDst = QString::fromStdString(rbool(sim->IDEX.RegDst));
-    QString MemRead = QString::fromStdString(rbool(sim->IDEX.MemRead));
-    QString MemtoReg = QString::fromStdString(rbool(sim->IDEX.MemtoReg));
-    QString ALUOp1 = QString::fromStdString(rbool(sim->IDEX.ALUOp1));
-    QString ALUOp0 = QString::fromStdString(rbool(sim->IDEX.ALUOp0));
-    QString MemWrite = QString::fromStdString(rbool(sim->IDEX.MemWrite));
-    QString ALUSrc = QString::fromStdString(rbool(sim->IDEX.ALUSrc));
-    QString RegWrite = QString::fromStdString(rbool(sim->IDEX.RegWrite));
 
+
+    //    QString Jump = QString::fromStdString("="+rbool(sim->ControlUnit.Jump));
+//    QGraphicsTextItem* gJump = this->circuitScene.addText(Jump);
+//    fontB(gJump);
+//    gJump->setPos(480, 212);
+    QString PCWrite = QString::fromStdString(rbool(sim->HazardUnit.PCWrite));
+    QGraphicsTextItem* gPCWrite = this->circuitScene.addText(PCWrite);
+    fontY(gPCWrite);
+    gPCWrite->setPos(65, 434);
+
+    QString NotStall = QString::fromStdString(rbool(sim->HazardUnit.notStall));
+    QGraphicsTextItem* gNotStall = this->circuitScene.addText(NotStall);
+    fontY(gNotStall);
+    gNotStall->setPos(180, 640);
+
+    QString Flush = QString::fromStdString(rbool(sim->flush));
+    QGraphicsTextItem* gFlush = this->circuitScene.addText(Flush);
+    fontB(gFlush);
+    gFlush->setPos(180, 640);
+
+    QGraphicsTextItem* gFlush2 = this->circuitScene.addText("IFFlush= "+Flush);
+    fontB(gFlush2);
+    gFlush2->setPos(368, 142);
+
+    QString Branch = QString::fromStdString("Branch= "  + rbool(sim->ControlUnit.Branch));
+    QGraphicsTextItem* gBranch = this->circuitScene.addText(Branch);
+    fontB(gBranch);
+    gBranch->setPos(480, 142);
+
+    QString PCSrc = QString::fromStdString(rbool(sim->PCSrc));
+    QGraphicsTextItem* gPCSrc = this->circuitScene.addText("= "+PCSrc);
+    fontB(gPCSrc);
+    gPCSrc->setPos(604, 16);
+
+    QGraphicsTextItem* gPCSrc2 = this->circuitScene.addText(PCSrc);
+    fontB(gPCSrc2);
+    gPCSrc2->setPos(170, 180);
+
+
+    QString Jump = QString::fromStdString(rbool(sim->ControlUnit.Jump));
+    QGraphicsTextItem* gJump = this->circuitScene.addText(Jump);
+    fontB(gJump);
+    gJump->setPos(480, 212);
+
+    QGraphicsTextItem* gJump2 = this->circuitScene.addText(Jump);
+    fontB(gJump2);
+    gJump2->setPos(120, 180);
+
+    QString ALUOp = QString::fromStdString("ALUOP = "+rbool(sim->IDEX.ALUOp1)+rbool(sim->IDEX.ALUOp0));
+    QGraphicsTextItem* gALUOp = this->circuitScene.addText(ALUOp);
+    fontB(gALUOp);
+    gALUOp->setPos(662, 253);
+//    QString ALUSrc = QString::fromStdString(rbool(sim->IDEX.ALUSrc));
+//    QGraphicsTextItem* gALUSrc = this->circuitScene.addText(ALUSrc);
+//    fontB(gALUSrc);
+//    gALUSrc->setPos(670, 146);
+
+    QString RegDst = QString::fromStdString("= " + rbool(sim->IDEX.RegDst));
+    QGraphicsTextItem* gRegDst1 = this->circuitScene.addText(RegDst);
+    fontB(gRegDst1);
+    gRegDst1->setPos(712, 232);
+    QGraphicsTextItem* gRegDst2 = this->circuitScene.addText(RegDst);
+    fontB(gRegDst2);
+    gRegDst2->setPos(817, 532);
+
+    QString MemToReg = QString::fromStdString("MemToReg = " + rbool(sim->IDEX.MemtoReg));
+    QGraphicsTextItem* gMemToReg = this->circuitScene.addText(MemToReg);
+    fontB(gMemToReg);
+    gMemToReg->setPos(670, 146);
+
+    QString RegWrite = QString::fromStdString("RegWrite = "+rbool(sim->IDEX.RegWrite));
     QGraphicsTextItem* gRegWrite = this->circuitScene.addText(RegWrite);
-    fontY(gRegWrite);
-    gRegWrite->setPos(580, 20);
+    fontB(gRegWrite);
+    gRegWrite->setPos(670, 166);
 
-//    gRegWrite->setPos(580, 20);
+    QString MemRead = QString::fromStdString(rbool(sim->IDEX.MemRead));
+    QGraphicsTextItem* gMemRead = this->circuitScene.addText("MemRead = "+MemRead);
+    fontB(gMemRead);
+    gMemRead->setPos(670, 192);
+    QGraphicsTextItem* gMemRead2 = this->circuitScene.addText("= "+MemRead);
+    fontB(gMemRead2);
+    gMemRead2->setPos(776, 112);
 
+    QString MemWrite = QString::fromStdString("MemWrite = "+rbool(sim->IDEX.MemWrite));
+    QGraphicsTextItem* gMemWrite = this->circuitScene.addText(MemWrite);
+    fontB(gMemWrite);
+    gMemWrite->setPos(670, 212);
 
 
     QString Data1 = QString::fromStdString(binToHex(sim->IDEX.Data1));
@@ -207,17 +361,40 @@ void MainWindow::refreshImgInfo(){
 
 
     // IF/ID Register
-    QString PC = QString::fromStdString(binToHex(sim->IFID.PC));
-    QString Inst = QString::fromStdString(binToHex(sim->IFID.Inst));
+    QString HRs = QString::fromStdString(sim->HazardUnit.Rs.to_string());
+    QGraphicsTextItem* gHRs = this->circuitScene.addText("rs= \n"+HRs);
+    gHRs->setPos(296, 140);
+    fontB(gHRs);
 
+    QString HRt = QString::fromStdString(sim->HazardUnit.Rt.to_string());
+    QGraphicsTextItem* gHRt = this->circuitScene.addText("rt= \n"+HRt);
+    gHRt->setPos(296, 100);
+    fontB(gHRt);
+
+    QString PC = QString::fromStdString(binToHex(sim->IFID.PC));
     QGraphicsTextItem* gPC = this->circuitScene.addText(PC);
     gPC->setPos(200, 300);
     fontB(gPC);
 
+    QString Inst = QString::fromStdString(binToHex(sim->IFID.Inst));
     QGraphicsTextItem* gInst = this->circuitScene.addText(Inst);
     gInst->setPos(200, 540);
     fontB(gInst);
-//    addText(Inst, 200, 540, "B");
+
+    QString BranchAdress = QString::fromStdString(binToHex(sim->branchAddress));
+    QGraphicsTextItem* gBranchAdress = this->circuitScene.addText("BranchAdress= "+BranchAdress);
+    gBranchAdress->setPos(160, -8);
+    fontB(gBranchAdress);
+
+    QString JumpAdress = QString::fromStdString(binToHex(sim->jumpAddress));
+    QGraphicsTextItem* gJumpAdress = this->circuitScene.addText("JumpAdress= "+JumpAdress);
+    gJumpAdress->setPos(160, 12);
+    fontB(gJumpAdress);
+
+    QString FetchedPC = QString::fromStdString(binToHex(sim->PC));
+    QGraphicsTextItem* gFetchedPC = this->circuitScene.addText(FetchedPC);
+    gFetchedPC->setPos(2, 572);
+    fontB(gFetchedPC);
 
     // After all
     this->update();
